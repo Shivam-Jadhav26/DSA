9921408cfe40e7e09d30c8eeceae5af3499edb0c
Shivam Jadhav <shivamaj2005@gmail.com>
2026-07-14 22:05:18 +0530

    Time: 3 ms (78.29%) | Memory: 46.2 MB (93.88%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,31 @@
+class Solution { 
+    public int romanToInt(String s) { 
+        int size = s.length(); 
+        int num = 0; 
+        
+        for (int i = 0; i < size; i++) { 
+            int val = getValue(s.charAt(i)); 
+            
+            
+            if (i + 1 < size && val < getValue(s.charAt(i + 1))) { 
+                num -= val; 
+            } else { 
+                num += val; 
+            } 
+        } 
+        return num; 
+    } 
+
+    private int getValue(char c) { 
+        switch (c) { 
+            case 'I': return 1; 
+            case 'V': return 5; 
+            case 'X': return 10; 
+            case 'L': return 50; 
+            case 'C': return 100; 
+            case 'D': return 500; 
+            case 'M': return 1000; 
+            default: return 0; 
+        } 
+    } 
+}
