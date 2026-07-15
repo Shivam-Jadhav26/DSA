456f5f8e28b5fcbb23cc284c880ce7b5e4078a56
Shivam Jadhav <shivamaj2005@gmail.com>
2026-07-15 22:30:36 +0530

    Time: 5 ms (82.64%) | Memory: 77.3 MB (48.98%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -5,16 +5,13 @@ class Solution {
         int right = height.length - 1;
 
         while (left < right) {
-            // Calculate the width between the pointers
+            
             int width = right - left;
             
-            // Find the shorter line
             int minHeight = Math.min(height[left], height[right]);
             
-            // Update maxArea if current container holds more water
             maxArea = Math.max(maxArea, minHeight * width);
 
-            // Move the pointer pointing to the shorter line inward
             if (height[left] < height[right]) {
                 left++;
             } else {
