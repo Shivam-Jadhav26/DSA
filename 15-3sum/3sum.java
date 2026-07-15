6f3d7f3dd9552333b55975417e45bdf4484438f0
Shivam Jadhav <shivamaj2005@gmail.com>
2026-07-15 23:43:02 +0530

    Time: 30 ms (88.67%) | Memory: 59 MB (85.71%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -10,17 +10,14 @@ class Solution {
         int size = nums.length;
         
         for (int i = 0; i < size - 2; i++) {
-            // If the current number is greater than 0, three positive numbers cannot sum to 0
             if (nums[i] > 0) {
                 break;
             }
             
-            // Skip duplicate values for the first element to avoid duplicate triplets
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
             
-            // Initialize two pointers for the remaining window
             int left = i + 1;
             int right = size - 1;
             
@@ -28,27 +25,21 @@ class Solution {
                 int sum = nums[i] + nums[left] + nums[right];
                 
                 if (sum == 0) {
-                    // Found a valid triplet
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     
-                    // Move both pointers inward
                     left++;
                     right--;
                     
-                    // Skip duplicates for the second element
                     while (left < right && nums[left] == nums[left - 1]) {
                         left++;
                     }
                     
-                    // Skip duplicates for the third element
                     while (left < right && nums[right] == nums[right + 1]) {
                         right--;
                     }
                 } else if (sum < 0) {
-                    // Sum is too small; move the left pointer right to increase the sum
                     left++;
                 } else {
-                    // Sum is too large; move the right pointer left to decrease the sum
                     right--;
                 }
             }
