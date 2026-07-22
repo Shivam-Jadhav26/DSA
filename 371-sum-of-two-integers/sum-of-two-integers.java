ca74295fb6c2df950b8ea3ee6b11becf62e0a4d1
Shivam Jadhav <shivamaj2005@gmail.com>
2026-07-22 21:11:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (14.98%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.java
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -1,5 +1,15 @@
 class Solution {
     public int getSum(int a, int b) {
-        return a+b;
+        while (b != 0) {
+        // 1. Find the carry bits where both a and b have 1s
+        int carry = a & b; 
+        
+        // 2. Sum the bits where a and b are different (XOR)
+        a = a ^ b; 
+        
+        // 3. Shift carry to the left to add it to the next higher bit
+        b = carry << 1; 
+    }
+    return a;
     }
 }
